d7b1b0ed0a2e6617c0113b10237bfa09d8f0cb96
Jeremiah Bassey Asuquo <31101804+jerryasuquo@users.noreply.github.com>
2020-03-26 01:52:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ console.log(name, "\n", courses);
 for (let i = 1; i <= courses.length; i++) {
   numberOfCourses = courses.length;
   var remainder = numberOfCourses % 2;
-
-  // looping through numbers between 1 - 200
-  for (let j = 1; j <= 200; j++) {
-    var jRem = j % 2;
-    if (remainder !== 0 && jRem !== 0) console.log(j);
-  }
+}
+// looping through numbers between 1 - 200
+for (let j = 1; j <= 200; j++) {
+  var jRem = j % 2;
+  if (remainder !== 0 && jRem !== 0) console.log(j);
 }
